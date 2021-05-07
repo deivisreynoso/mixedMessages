aff0b9b29dd248f14a4f16bfeb39382c5c4991ff
Deivis Reynoso <deivisreynoso@Deiviss-iMac.lan>
2021-05-07 19:01:46 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const inspiration = [
+  "The Best Way To Get Started Is To Quit Talking And Begin Doing.",
+  "The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty.",
+  "Don’t Let Yesterday Take Up Too Much Of Today.",
+  "You Learn More From Failure Than From Success. Don’t Let It Stop You. Failure Builds Character.",
+  "It’s Not Whether You Get Knocked Down, It’s Whether You Get Up.",
+  "If You Are Working On Something That You Really Care About, You Don’t Have To Be Pushed. The Vision Pulls You.",
+  "People Who Are Crazy Enough To Think They Can Change The World, Are The Ones Who Do.",
+  "Failure Will Never Overtake Me If My Determination To Succeed Is Strong Enough.",
+  "Entrepreneurs Are Great At Dealing With Uncertainty And Also Very Good At Minimizing Risk. That’s The Classic Entrepreneur.",
+  "We May Encounter Many Defeats But We Must Not Be Defeated.",
+];
+
+const randomQuote = (arr) => {
+  const randomNumber = Math.floor(Math.random() * 9);
+  return arr[randomNumber];
+};
+
+console.log(randomQuote(inspiration));
